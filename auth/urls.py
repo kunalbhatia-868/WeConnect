eecc5cb7e8377b4137ca5176e37d300d353776c0
kunal <kunalbhatia868@gmail.com>
2021-10-26 16:01:16 +0530

    login logout signup views added with their templates and css

--- auth/urls.py
+++ auth/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from auth import views
 
 urlpatterns=[
-    
+    path('login/',views.logIn,name='login'),
+    path('logout/',views.logOut,name='logout'),
+    path('signup/',views.register,name='signup'),
+
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from auth.forms import SignUpForm
 # Create your views here.
+
+def logIn(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,f"{user.username} has been logged in ")
+        else:    
+            messages.info(request,f"username or password didn't match")    
+
+    return render(request,'auth/login.html')
+
+
+def logOut(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    if request.method=='POST':
+        form=SignUpForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+    context={
+        'form':SignUpForm,
+    }
+    return render(request,'auth/signup.html',context)
