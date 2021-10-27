96f1c56c5a32e94c1ac96e591fc113b444d0ea56
kunal <kunalbhatia868@gmail.com>
2021-10-27 20:23:24 +0530

    friends list Page and Friends Detail Page Added

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from main import views
 
 urlpatterns=[
     path('',views.Wall.as_view(),name="wall"),
-    path('home',views.Home.as_view(),name="home"),    
-    path('create/',views.CreatePost.as_view(),name="create_post")
+    path('home/',views.Home.as_view(),name="home"),    
+    path('create/',views.CreatePost.as_view(),name="create_post"),
+    path('friends/',views.Friends.as_view(),name="friends"),
+    path('friends/<int:pk>/',views.UserDetail.as_view(),name="friend_detail")
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from main import models,forms
 from django.contrib import messages
@@ -14,7 +15,7 @@ class Wall(LoginRequiredMixin,ListView):
         friendIds=[friend.person2.id for friend in models.Friend.objects.filter(person1=self.request.user)]
         friendIds+=[friend.person1.id for friend in models.Friend.objects.filter(person2=self.request.user)]
         
-        return models.Post.objects.filter(user__in=friendIds)
+        return models.Post.objects.filter(user__in=friendIds).order_by('-created_on')
 
     def get_context_data(self, **kwargs):
         context_data=super().get_context_data(**kwargs)
@@ -27,10 +28,11 @@ class Home(LoginRequiredMixin,ListView):
     login_url="login"
 
     def get_queryset(self):
-        return models.Post.objects.filter(user=self.request.user)
+        return models.Post.objects.filter(user=self.request.user).order_by('-created_on')
     
 
-class CreatePost(CreateView):
+class CreatePost(LoginRequiredMixin,CreateView):
+    login_url='login'
     def post(self,request):
         form=forms.PostForm(request.POST,request.FILES)
         if form.is_valid():
@@ -42,3 +44,17 @@ class CreatePost(CreateView):
             messages.info(f" Please Enter valid Entries")
         
     
+class Friends(LoginRequiredMixin,ListView):
+    template_name="main/friends.html"
+    context_object_name="friends"
+    login_url='login'
+
+    def get_queryset(self):
+        friendIds=[friend.person2 for friend in models.Friend.objects.filter(person1=self.request.user)]
+        friendIds+=[friend.person1 for friend in models.Friend.objects.filter(person2=self.request.user)]
+        return friendIds
+
+class UserDetail(LoginRequiredMixin,DetailView):
+    template_name="main/detail.html"
+    model=User
+    login_url='login'
